19748c91cc4fc4bf8dda13bebf3392c0e5f2d2c5
Wu <jio12138@qq.com>
2025-04-13 21:24:29 +0800

    添加 .gitignore 文件，更新缓存和输出目录配置，修改 hello_world.py 中的类名和文本显示方式

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,7 +1,12 @@
 from manimlib import *
+from os import system
 
-class HelloWorld(Scene):
+class TextDemo(Scene):
     def construct(self):
-        text= Tex("Hello, World!")
-        self.play(Write(text))
+        tex = Text("Hello, World!", font_size=72, color=WHITE)
+        animation = Write(tex)
+        self.play(animation)
         self.wait(1)
+
+if __name__ == '__main__':
+    system('manimgl {} TextDemo --config_file ../custom_config.yml'.format(__file__))
